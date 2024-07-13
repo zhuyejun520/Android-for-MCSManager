9dcac1a8c6dfe8ea3584f5f3cec553557f361948
zhuyejun520 <134207796+zhuyejun520@users.noreply.github.com>
2024-07-13 19:20:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ echo "安装MCSM中..."
 cd ~
 mkdir mcsm
 cd ~/mcsm
-git clone https://kkgithub.com/MCSManager/MCSManager-Web-Production.git
+git clone https://github.com/MCSManager/MCSManager-Web-Production.git
 mv MCSManager-Web-Production web
 cd web
 npm install --production --registry=https://registry.npmmirror.com/
 cd ~/mcsm
 echo "安装守护进程中..."
-git clone https://kkgithub.com/MCSManager/MCSManager-Daemon-Production.git
+git clone https://github.com/MCSManager/MCSManager-Daemon-Production.git
 mv MCSManager-Daemon-Production daemon
 cd daemon
 npm install --production --registry=https://registry.npmmirror.com/
